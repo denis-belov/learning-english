27dba59968e8a477f729ecc49006bb52e2ae36d0
Denis Belov <dsbelov63@gmail.com>
2021-02-06 10:14:19 -0500

    up

--- webpack.config.js
+++ webpack.config.js
@@ -5,38 +5,38 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const host = child_process.execSync('ipconfig', { encoding: 'utf8' }).split('\r\n').slice(-4, -3)[0].split(':')[1].trim();
 
 module.exports = {
-  entry: './src/index.js',
+	entry: './src/index.js',
 
-  resolve: {
-    extensions: [ '.js' ],
-    modules: [ './node_modules' ],
-  },
+	resolve: {
+		extensions: [ '.js' ],
+		modules: [ './node_modules' ],
+	},
 
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'index.js',
-    publicPath: '/',
-  },
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'index.js',
+		publicPath: '/',
+	},
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: path.join(__dirname, 'build/index.html'),
-      template: path.join(__dirname, 'src/index.html'),
-      inject: false,
-      minify: {
-        removeAttributeQuotes: true,
-      },
-    }),
-  ],
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: path.join(__dirname, 'build/index.html'),
+			template: path.join(__dirname, 'src/index.html'),
+			inject: false,
+			minify: {
+				removeAttributeQuotes: true,
+			},
+		}),
+	],
 
-  devServer: {
-    host: '0.0.0.0',
-    port: 8088,
-    compress: true,
-    // watchOptions: {
-    //   ignored: [
-    //     path.join(__dirname, 'backend'),
-    //   ]
-    // }
-  },
+	devServer: {
+		host: '0.0.0.0',
+		port: 8088,
+		compress: true,
+		// watchOptions: {
+		//   ignored: [
+		//     path.join(__dirname, 'backend'),
+		//   ]
+		// }
+	},
 };
