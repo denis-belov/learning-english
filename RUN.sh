e45c091e91f01b7719590d10b157490d625db603
Denis Belov <dsbelov63@gmail.com>
2021-05-31 15:32:07 -0400

    up

--- RUN.sh
+++ RUN.sh
@@ -1,1 +1,1 @@
-~/.nvm/versions/node/v14.4.0/bin/node /home/denis/reps/denis-belov/learning-english/node_modules/webpack-dev-server/bin/webpack-dev-server.js --open
+/home/denis/.nvm/versions/node/v14.4.0/bin/node $(pwd)/node_modules/webpack-dev-server/bin/webpack-dev-server.js --open
